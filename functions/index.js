ec96e118e37005f36d5b85cf597251d5688ff457
MounicaReddy07 <mounicareddyk2001@gmail.com>
2021-05-25 18:31:48 +0530

    First Commit

---
+++ functions/index.js
@@ -0,0 +1,35 @@
+const functions = require("firebase-functions");
+const app = require("express")();
+const {admin,db}  = require("./utils/admin")
+
+app.post("/createUser",(req,res)=>{
+    const inputs = req.body;
+    return admin.auth().createUser({
+        email:inputs.email,
+        password:inputs.password
+    })
+    .then((user)=>{
+        req.user = user
+        return admin.auth().setCustomUserClaims(user.uid,{role:"user"})
+    })
+    .then(()=>{
+        const userRef = db.collection("USERS").doc(req.user.uid)
+        return userRef.set({
+            ...inputs,
+            isExist:true,
+            createdAt:admin.firestore.FieldValue.serverTimestamp(),
+            role:"user",
+            id:userRef.id,
+            uid:req.user.uid,
+        })
+    })
+    .then(()=>{
+        return res.send("User created successfully")
+    })
+    .catch((err)=>{
+        console.err(err)
+        return res.send("Failed to create a user")
+    })
+})
+
+exports.api=functions.https.onRequest(app);

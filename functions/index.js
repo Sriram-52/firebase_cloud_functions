11bb1cfcac5d6d009802725e3253062712be1585
OLETISATISHKUMAR <satisholeti1020@gmail.com>
2021-05-26 16:52:57 +0530

    day-1

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,37 @@
 const functions = require("firebase-functions");
+const app=require("express")();
+const { admin, db }=require("./uitls/admin")
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+app.post("/createUser",(req ,res) => {
 
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });
+    const inputs = req.body;
+    return admin.auth().createUser({
+        email:inputs.email,
+        password:inputs.password
+    })
+     .then((user) =>{
+         req.user=user;
+         return admin.auth().setCustomUserClaims(user.uid,{role:"username"})
+    })
+    .then(() =>{
+       const userRef = db.collection("USERS").doc(req.user.uid)
+       return userRef
+       .set({
+           ...inputs,
+           isExist: true,
+           createdAt:admin.firestore.FieldValue.serverTimestamp(),
+           role:"user",
+           id:req.user.uid,
+           uid:req.user.uid
+       })
+    })
+    .then(() =>{   
+        return res.send("user created sucessfuly")
+    })
+    .catch((err) =>{
+        console.error(err)
+        return res.send("fail to create useer")
+       })
+    })
+
+exports.api=functions.https.onRequest(app)

5cf98b79efd7e57cce23f2f7e33fdbf380607af7
Your Name <you@example.com>
2021-05-26 14:07:36 +0530

    first commit

---
+++ functions/index.js
@@ -0,0 +1,47 @@
+const functions = require("firebase-functions");
+const app=require("express")();
+const { admin, db }=require("./uitls/admin")
+
+app.post("/createUser",(req ,res) =>
+{
+    const inputs = req.body;
+  
+    const promise= admin.auth().createUser({
+        email:inputs.email,
+        password:inputs.password
+    }) .then((user) =>
+    {
+         req.user=user;
+           return admin.auth().setCustomUserClaims(user.uid,{role:"username"})
+    }).then(() =>
+    {
+       const userRef = db
+        .collection("USERS")
+        .doc(req.user.uid)
+
+
+       return userRef
+       .set({
+           ...inputs,
+           isExist: true,
+           createdAt:admin.firestore.FieldValue.serverTimestamp(),
+           role:"user",
+           id:req.user.uid,
+           uid:req.user.uid
+       })
+    })
+    .then(() =>{
+               
+        return res.send("user created sucessfuly")
+    })
+       .catch((err) =>
+       {
+           console.error(err)
+           return res.send("fail to create useer")
+       })
+    })
+   
+
+
+
+exports.api=functions.https.onRequest(app);
